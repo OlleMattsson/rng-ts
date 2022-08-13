9477fe6aa09d83097bcd75f7549f6bfcfa95b045
Olle Mattsson <olle.mattsson@gmail.com>
2022-08-14 01:51:18 +0300

    add tests

--- src/tests.ts
+++ src/tests.ts
@@ -2,13 +2,46 @@ import test, {describe, it} from "node:test"
 import assert from "node:assert"
 import {RNG} from "."
 
-describe('Create a random number generator', () => {
-
+describe('new RNG()', () => {
     const rng = new RNG()
 
     it ("should have a getter", () => {
-        assert.ok(rng.get());
+        const typeofGet = typeof rng.get()
+        const expectedTypeofGet = "object";
+        assert.strictEqual(typeofGet, expectedTypeofGet)
     })
 
+    it ("should have a constructor and get method", () => {
+        const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(rng))
+        const expectedMethods = ["constructor", "get"]
+        assert.deepEqual(methods, expectedMethods)
+    })
+
+})
+
+
+
+describe('RNG.get() should return one random number', () => {
+    const rng = new RNG()
+
+    assert.ok(rng.get());
+})
+
+describe('RNG.get({number})', () => {
+
+    const rng = new RNG()
     
+    it ("should throw if count < 1", () => {
+        assert.rejects( async () => await rng.get({count: 0}), {
+            name: 'Error',
+            message: "invalid count"
+        });
+    })
+
+    it ("should throw if count > 1024", () => {
+        assert.rejects( async () => await rng.get({count: 0}), {
+            name: 'Error',
+            message: "invalid count"
+        });
+    })
 })
